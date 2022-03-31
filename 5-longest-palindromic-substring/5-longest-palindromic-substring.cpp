a858da0dcedcdb2221f00f7af71f4aab87e6569a
Rehman Khan <66064740+Shutah64@users.noreply.github.com>
2022-03-31 19:40:14 +0500

    Time: 225 ms (48.03%), Space: 231.8 MB (14.99%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -17,27 +17,21 @@ void middle(string s, int l, int r, int &e, int &b, int &mx)
 class Solution {
 public:
     string longestPalindrome(string s) {
-    int mx1 = 1;
-    int b1 = 0;
-    int e1 = 0;
-    int mx2 = 1;
-    int b2 = 0;
-    int e2 = 0;
+    int mx = 1;
+    int b = 0;
+    int e = 0;
+    
     for (int i = 0; i < s.length(); i++)
     {
-        middle(s, i, i + 1, e1, b1, mx1);
-        middle(s, i, i, e2, b2, mx2);
+        middle(s, i, i + 1, e, b, mx);
+        middle(s, i, i, e, b, mx);
     }
-    string st1 = "";
-    for (int i = b1; i <= e1; i++)
+    string st = "";
+    for (int i = b; i <= e; i++)
     {
-        st1 += s[i];
+        st += s[i];
     }
-    string st2 = "";
-    for (int i = b2; i <= e2; i++)
-    {
-        st2 += s[i];
-    }
-    return st1.length() > st2.length() ? st1 : st2;
+    
+    return st;
     }
 };
